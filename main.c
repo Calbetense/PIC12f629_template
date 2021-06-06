8ca47a987e155a6a63214a4eefb042145e11c94e
Vermelho <daninc12@gmail.com>
2021-06-06 06:51:30 -0300

    Template for PIC12F629

---
+++ main.c
@@ -0,0 +1,37 @@
+
+// PIC12F629 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+#ifndef _XTAL_FREQ
+ // Unless already defined assume 4MHz system frequency
+ // This definition is required to calibrate __delay_us() and __delay_ms()
+ #define _XTAL_FREQ 4000000
+#endif
+
+void main(void) {
+    //OSCCAL	= 0x3ff;    // Cristal interno a 4MHz
+    TRISIO = 0b00001000;
+    while (1) {
+        GPIO = 0b11110111;
+        __delay_ms(500);
+        GPIO = 0b00000000;
+        __delay_ms(500);
+    }
+
+    return;
+}
